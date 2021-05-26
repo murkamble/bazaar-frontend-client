dc556a7c2d6e1090ac4d14a5791b9a1ebfcd688b
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-26 13:08:29 +0530

    urlConfig update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,13 @@
-export const api = 'https://server-backend-rest-api.herokuapp.com/api'
+// export const api = 'https://server-backend-rest-api.herokuapp.com/api'
+// export const generatePublicUrl = (fileName) => {
+//     return `${fileName}`
+// }
+
+const baseUrl = "https://bazaar-by-mayur-kamble.herokuapp.com"
+
+export const api = `${baseUrl}/api`
+
 export const generatePublicUrl = (fileName) => {
-    return `https://server-backend-rest-api.herokuapp.com/public/${fileName}`
+    // return `${baseUrl}/public/${fileName}`
+    return `${fileName}`
 }
