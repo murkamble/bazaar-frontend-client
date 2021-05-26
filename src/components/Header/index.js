d13fcfe23d75bf9ac5b9b39d54e9c76f76dd7f40
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-26 14:21:03 +0530

    Header Update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -113,7 +113,7 @@ const Header = (props) => {
             //   setLoginModal(true);
             // }}
           >
-            Manu
+            Menu
           </a>
         }
         menus={[
